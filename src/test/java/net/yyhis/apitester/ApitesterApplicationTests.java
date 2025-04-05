b439e50dd4f6d8ef62af72518d84dc22955008d8
yyhis <oouoo717@gmail.com>
2025-04-05 20:05:18 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/net/yyhis/apitester/ApitesterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yyhis.apitester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
